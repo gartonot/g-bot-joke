843c562f8b21726e0317a9a636d1cf83c8f8ab29
gartonot <gartonot62@gmail.com>
2024-09-29 22:29:44 +0300

    Регистрирует новую клавиатуру

--- app/modules/register-keyboards.js
+++ app/modules/register-keyboards.js
@@ -1,11 +1,13 @@
 import {
     startKeyboardByAdmin,
     startKeyboardByUser,
+    settingsJokeByAdmin,
 } from '../keyboards/index.js';
 
 const registerKeyboards = (bot) => {
     bot.use(startKeyboardByAdmin);
     bot.use(startKeyboardByUser);
+    bot.use(settingsJokeByAdmin);
 }
 
 export default registerKeyboards;
